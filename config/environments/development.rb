360ed37da5d9dea7f741f5bef31d726f82231126
LIMITLESS <pempun@PEMPUNs-Mac-mini.local>
2019-03-01 16:02:34 -0500

    Removed development bug

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -50,7 +50,7 @@ Rails.application.configure do
 
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
-  config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+  # config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
   # Mailer config
   config.action_mailer.raise_delivery_errors = true
